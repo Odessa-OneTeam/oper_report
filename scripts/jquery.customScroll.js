33b084803fe7df2fd8436f3c6dd1e1a65b49e05f
blu2z <blu2z@yandex.ru>
2015-05-11 17:58:02 +0300

    update

--- scripts/jquery.customScroll.js
+++ scripts/jquery.customScroll.js
@@ -1,8 +1,10 @@
 (function($){
     $(window).load(function() {
     	$.mCustomScrollbar.defaults.scrollButtons.enable=true;
+    	$.mCustomScrollbar.defaults.scrollButtons.enable=true;
     	$(".wrap").mCustomScrollbar({
 		    scrollInertia: 0,
+		    alwaysShowScrollbar: 1,
 		    theme: "oper-report"
 		});
 
